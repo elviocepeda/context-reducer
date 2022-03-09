db90472c7870ec58dc46dd3d117a075de01b32e8
elviocepeda <cepedaelvio@gmail.com>
2022-03-09 01:16:13 -0300

    comm01

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Delete from "./svg/Delete";
+import Edit from "./svg/Edit";
 
 const ProductCard = ({ product }) => {
 
   return (
     <div className="product-card">
-      <img src={product.imgUrl} alt={product.title} />
+      <div className="card-img">
+        <img src={product.imgUrl} alt={product.title} />
+      </div>
+      <div className="card-body">
       <h3>{product.title}</h3>
       <p>{product.description}</p>
+        <div className="btn-container">
+          <Edit />
+          <Delete />
+        </div>
+
+      </div>
     </div>
   );
 };
---
+++ src/components/svg/Delete.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Delete = () => {
+  return (
+    <svg
+      width="25px"
+      height="25px"
+      enable-background="new 0 0 24 24"
+      id="Layer_1"
+      version="1.0"
+      viewBox="0 0 24 24"
+    >
+      <g fill="#d72503">
+        <path d="M12,2C6.5,2,2,6.5,2,12c0,5.5,4.5,10,10,10s10-4.5,10-10C22,6.5,17.5,2,12,2z M16.9,15.5l-1.4,1.4L12,13.4l-3.5,3.5   l-1.4-1.4l3.5-3.5L7.1,8.5l1.4-1.4l3.5,3.5l3.5-3.5l1.4,1.4L13.4,12L16.9,15.5z" />
+      </g>
+    </svg>
+  );
+};
+
+export default Delete;
